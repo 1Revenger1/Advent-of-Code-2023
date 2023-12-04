68944ae03dbd627b48867fc37a153707dd0d03c6
Avery Black <avery.c.black@gmail.com>
2023-12-04 10:22:45 -0800

    Remove warnings

--- src/days/day07.rs
+++ src/days/day07.rs
@@ -6,6 +6,7 @@ pub fn solve(str: String) -> SolutionPair {
     // Your solution here...
     let sol1: u64 = 0;
     let sol2: u64 = 0;
+    let _ = str;
 
     (Solution::from(sol1), Solution::from(sol2))
 }
