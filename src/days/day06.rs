16355a0bcd112842bd9ebb39f6f48565ac53b900
Avery Black <avery.c.black@gmail.com>
2023-12-05 21:27:09 -0800

    Day 6

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,12 +1,50 @@
 use crate::{Solution, SolutionPair};
 
-///////////////////////////////////////////////////////////////////////////////
-
 pub fn solve(str: String) -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
-    let _ = str;
+    let mut line_iter = str.lines();
+    let times: Vec<u64> = line_iter
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let distances: Vec<u64> = line_iter
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .skip(1)
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let sol1: u64 = times.iter().zip(distances.iter())
+        .map(|(time, record)| {
+            (1..*time)
+                .filter(|speed| {
+                    let time_left = time - speed;
+                    time_left * speed > *record
+                })
+                .count() as u64
+        })
+        .product();
+
+    let total_time: u64 = times.iter().fold(0, |accum, cur_val| {
+        let mult = cur_val.checked_ilog10().unwrap() + 1;
+        (accum * u64::pow(10, mult)) + cur_val
+    });
+
+    let total_distance: u64 = distances.iter().fold(0, |accum, cur_dis| {
+        let mult = cur_dis.checked_ilog10().unwrap() + 1;
+        (accum * u64::pow(10, mult)) + cur_dis
+    });
+
+    let sol2 = (1..total_time)
+        .filter(|speed| {
+            let time_left = total_time - speed;
+            time_left * speed > total_distance
+        })
+        .count();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
