cc1c373dc6569206b99fdcb76369c8051f851f29
Avery Black <avery.c.black@gmail.com>
2023-12-07 22:02:40 -0800

    Simplify and optimize

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -2,67 +2,50 @@ use std::collections::HashMap;
 
 use crate::{Solution, SolutionPair, etc::math::lcm};
 
-pub fn solve(str: String) -> SolutionPair {
-    let mut lines = str.lines();
-    let dirs: Vec<char> = lines.next().unwrap().chars().collect();
-
-    let nodes: HashMap<String, (String, String)> = lines.skip(1).map(|line| {
-        let node = String::from(&line[0..3]);
-        let left = String::from(&line[7..10]);
-        let right = String::from(&line[12..15]);
-        (node, (left, right))
-    }).collect();
-
-    // Part 1
-    let mut node = String::from("AAA");
+fn calc_steps(dirs: &Vec<char>, nodes: &HashMap<&str, (&str, &str)>, start_node: &str) -> u64 {
+    let mut node = start_node;
     let mut steps = 0;
-    loop {
+    while !node.ends_with('Z') {
         let dir = dirs[steps % dirs.len()];
         let choices = &nodes[&node];
 
-        if node == "ZZZ" {
-            break;
-        }
-
         node = if dir == 'L' {
-            choices.0.clone()
+            choices.0
         } else {
-            choices.1.clone()
+            choices.1
         };
 
         steps += 1;
     }
 
+    steps as u64
+}
+
+pub fn solve(str: String) -> SolutionPair {
+    let mut lines = str.lines();
+    let dirs: Vec<char> = lines.next().unwrap().chars().collect();
+
+    let nodes: HashMap<&str, (&str, &str)> = lines.skip(1).map(|line| {
+        let node = &line[0..3];
+        let left = &line[7..10];
+        let right = &line[12..15];
+        (node, (left, right))
+    }).collect();
+
+    // Part 1
+    let steps = calc_steps(&dirs, &nodes, "AAA");
+
     // Part 2
-    let cur_nodes: Vec<String> = nodes.iter().filter_map(|(key, _)| {
+    let cur_nodes: Vec<&str> = nodes.iter().filter_map(|(key, _)| {
         if key.ends_with('A') {
-            Some(key.clone())
+            Some(*key)
         } else {
             None
         }
     }).collect();
 
-    let cycle_lengths: Vec<u64> = cur_nodes.iter().map(|start_node| {
-        let mut node = start_node.clone();
-        let mut steps = 0;
-        loop {
-            let dir = dirs[steps % dirs.len()];
-            let choices = &nodes[&node];
-    
-            if node.ends_with('Z') {
-                break;
-            }
-    
-            node = if dir == 'L' {
-                choices.0.clone()
-            } else {
-                choices.1.clone()
-            };
-    
-            steps += 1;
-        }
-        
-        steps as u64
+    let cycle_lengths: Vec<u64> = cur_nodes.into_iter().map(|start_node| {
+        calc_steps(&dirs, &nodes, start_node)
     }).collect();
 
     (Solution::from(steps), Solution::from(lcm(cycle_lengths)))
